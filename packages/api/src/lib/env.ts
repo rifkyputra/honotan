ff8757c98d2364eb69cc32b5f90b08c80106b641
rifky adni putra <rifqempul@gmail.com>
2025-10-20 18:48:44 +0800

    modify env and package.json

--- packages/api/src/lib/env.ts
+++ packages/api/src/lib/env.ts
@@ -1,15 +1,25 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import { z } from 'zod';
+import { z } from "zod";
 
 const envSchema = z.object({
-    PORT: z.string().default('3000'),
-    LOG_LEVEL: z.string().default('info'),
-    NODE_ENV: z.enum(['development', 'production']).default('development'),
-    SECRET_KEY: z.string(),
-    REDIS_HOST: z.string().default('localhost'),
-    REDIS_PORT: z.string().default('6379'),
-    DATABASE_URL: z.string(),
+  PORT: z.string().default("3000"),
+  LOG_LEVEL: z.string().default("info"),
+  NODE_ENV: z.enum(["development", "production"]).default("development"),
+  SECRET_KEY: z.string(),
+  REDIS_HOST: z.string().default("localhost"),
+  REDIS_PORT: z.string().default("6379"),
+  DATABASE_URL: z.string(),
 });
 
-export default envSchema.parse(process.env);
+const env = process.env;
+
+const modifiedEnv = {
+  ...env,
+  PORT: env.PORT ?? "3000",
+  LOG_LEVEL: env.LOG_LEVEL ?? "info",
+  NODE_ENV: env.NODE_ENV ?? "development",
+  DATABASE_URL: `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@localhost:5432/${env.POSTGRES_DB}`,
+};
+
+export default envSchema.parse(modifiedEnv);
